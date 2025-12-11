4fa5027a879f3f3b57393be47360dab8dc51326e
Conrad Peyer <conrad.peyer@srf.ch>
2025-12-11 23:34:55 +0100

    improve template

--- src/template/DayN.kt
+++ src/template/DayN.kt
@@ -1,11 +1,24 @@
 package template
 
 import java.io.File
+import kotlin.time.measureTime
 
 private val input = File("./src/dayN/sample_input.txt")
     .readText()
 
 
 fun main() {
+    println("Day N - ???")
 
+    measureTime {
+        println("Part I: ???")
+    }.also {
+        println("Execution time: $it")
+    }
+
+    measureTime {
+        println("Part II: ???")
+    }.also {
+        println("Execution time: $it")
+    }
 }
